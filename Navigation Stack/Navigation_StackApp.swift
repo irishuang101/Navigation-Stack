df21d07f40b2f7b7682eeaf4accee839d707a76c
irishuang101 <26huangi49@stu.smuhsd.org>
2023-07-14 09:21:31 -0700

    Initial Commit

---
+++ Navigation Stack/Navigation_StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_StackApp.swift
+//  Navigation Stack
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
